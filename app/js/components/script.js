d618cd32ccc9899207e9e1eaa5a95c0565ba5016
DimaMelnichuk <57269449+DimaMelnichuk@users.noreply.github.com>
2021-01-22 11:31:42 +0300

    Add aside and content-page with adaptive

--- app/js/components/script.js
+++ app/js/components/script.js
@@ -2,7 +2,7 @@ let mainSlider = new Swiper('.main-slider', {
 	loop: true,
 	slidesPerView: 1,
 	grabCursor: true,
-	speed: 1000,
+	speed: 500,
 	effect: 'fade',
 
 /* 	autoplay: {
@@ -12,6 +12,45 @@ let mainSlider = new Swiper('.main-slider', {
 	pagination: {
 		el: '.main-slider__pagination',
 		clickable: true,
+	 },
+
+});
+
+let lifestyleSlider = new Swiper('.lifestyle-page__slider-container', {
+	loop: false,
+	slidesPerView: 1,
+	effect: 'fade',
+	slidesPerColumnFill: "row",
+	grabCursor: true,
+	speed: 500,
+	
+	navigation: {
+		nextEl: '.lifestyle-page__button-next',
+		prevEl: '.lifestyle-page__button-prev',
+	 },
+
+	 breakpoints: {
+		// when window width is >= 320px
+		320: {
+			slidesPerColumn: 1,
+		},
+		// when window width is >= 640px
+		600: {
+			slidesPerColumn: 3,
+		}
+	 }
+});
+
+let fashionSlider = new Swiper('.fashion-page__slider-container', {
+	loop: false,
+	slidesPerView: 1,
+	grabCursor: true,
+	speed: 500,
+	effect: 'fade',
+
+	navigation: {
+		nextEl: '.fashion-page__slider-next',
+		prevEl: '.fashion-page__slider-prev',
 	 },
 
 });
